2c5731ef94b68a0be406080c9b011eafc0cfbe99
Musakalamz <ogunsolumusa@gmail.com>
2025-02-02 10:49:11 +0100

    Navigating between pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>The Wild Oasis, welcome on board</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis, welcome on board</h1>;
+      <Link href="/cabins">Explore luxury</Link>
+    </div>
+  );
 }
