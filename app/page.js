2ddbc3f2f4e5d7d0c659e8a9b97484371057edac
Musakalamz <ogunsolumusa@gmail.com>
2025-02-02 11:26:19 +0100

    Creating Layout and Logo component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis, welcome on board</h1>;
-      <Link href="/cabins">Explore luxury</Link>
+      <Link href="/cabins">Explore luxury cabins</Link>
     </div>
   );
 }
